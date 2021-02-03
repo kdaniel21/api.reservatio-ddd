0a50c33176961aec5b56c6dcf4c3453ee1655724
Daniel Kiss <kissd621@gmail.com>
2021-02-03 13:55:19 +0100

    Added UserName tests

--- src/modules/users/domain/UserName.spec.ts
+++ src/modules/users/domain/UserName.spec.ts
@@ -1,1 +1,34 @@
-describe('UserName value object', () => {})
+import faker from 'faker'
+import UserName, { InvalidUserNameError } from './UserName'
+
+describe('UserName value object', () => {
+  it('should create a valid UserName object', () => {
+    const name = faker.name.findName()
+
+    const userNameOrError = UserName.create(name)
+
+    expect(userNameOrError.isSuccess()).toBe(true)
+    expect(userNameOrError.isFailure()).toBe(false)
+    expect(userNameOrError.value?.value).toBe(name)
+  })
+
+  it('should fail when using a too long name', () => {
+    const name = 'This a very very long name that faker cant generate'
+
+    const userNameOrError = UserName.create(name)
+
+    expect(userNameOrError.isSuccess()).toBe(false)
+    expect(userNameOrError.isFailure()).toBe(true)
+    expect(userNameOrError.value).toBeInstanceOf(InvalidUserNameError)
+  })
+
+  it('should fail when passing in an empty string', () => {
+    const name = ''
+
+    const userNameOrError = UserName.create(name)
+
+    expect(userNameOrError.isSuccess()).toBe(false)
+    expect(userNameOrError.isFailure()).toBe(true)
+    expect(userNameOrError.value).toBeInstanceOf(InvalidUserNameError)
+  })
+})
