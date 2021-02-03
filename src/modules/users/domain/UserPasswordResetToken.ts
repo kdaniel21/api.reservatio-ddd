60c51c929d6d24c31d96e70c0c39cbe23e646bb5
Daniel Kiss <kissd621@gmail.com>
2021-02-03 14:52:55 +0100

    Extended TokenValueObject as UserPasswordResetToken

--- src/modules/users/domain/UserPasswordResetToken.ts
+++ src/modules/users/domain/UserPasswordResetToken.ts
@@ -1,12 +1,22 @@
 import { ErrorOr } from '@shared/core/DomainError'
-import { Guard } from '@shared/core/Guard'
 import { Result } from '@shared/core/Result'
-import TokenValueObject from '@shared/domain/TokenValueObject'
-import ValueObject from '@shared/domain/ValueObject'
-import TextUtils from '@shared/utils/TextUtils'
-import { MissingArgumentError } from './User'
+import { TokenValueObject, TokenValueObjectProps } from '@shared/domain/TokenValueObject'
 
 export default class UserPasswordResetToken extends TokenValueObject {
   static TOKEN_LENGTH = 30
   static EXPIRATION_HOURS = 12
+
+  private constructor(public props: TokenValueObjectProps) {
+    super(props)
+  }
+
+  static create(props: TokenValueObjectProps): ErrorOr<UserPasswordResetToken> {
+    const validPropsOrError = TokenValueObject.createValueObject(props)
+
+    if (validPropsOrError.isFailure()) return Result.fail(validPropsOrError.value)
+
+    const validProps = validPropsOrError.value as TokenValueObjectProps
+    const userPasswordResetToken = new UserPasswordResetToken(validProps)
+    return Result.ok(userPasswordResetToken)
+  }
 }
