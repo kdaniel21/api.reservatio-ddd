ff5e4ca6481600b13f82c398b38ccd69303169b4
Daniel Kiss <kissd621@gmail.com>
2021-02-16 17:40:38 +0100

    Added UnexpectedError

--- src/shared/core/AppError.ts
+++ src/shared/core/AppError.ts
@@ -1,6 +1,12 @@
 import { DomainError } from './DomainError'
 
 export namespace AppError {
+  export class UnexpectedError extends DomainError {
+    constructor() {
+      super({ message: 'Something went wrong unexpectedly. Please try again!' })
+    }
+  }
+
   export class UndefinedArgumentError extends DomainError {
     constructor(message: string) {
       super({ message })
