3cba6f698baf2b32db5f8266b2ce2957d49670c9
Daniel Kiss <kissd621@gmail.com>
2021-02-19 08:11:11 +0100

    Added router prefix

--- src/shared/infra/http/api/v1Router.ts
+++ src/shared/infra/http/api/v1Router.ts
@@ -3,6 +3,8 @@ import usersRouter from '@modules/users/infra/http/api/routes'
 
 const router = new Router()
 
+router.prefix('/api/v1')
+
 router.use(usersRouter.routes(), usersRouter.allowedMethods())
 
 export default router
