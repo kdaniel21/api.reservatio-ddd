cbfac7310fad5d8f547c9670cef7aac64bd112b0
Daniel Kiss <kissd621@gmail.com>
2021-02-19 08:28:15 +0100

    Added morgan to log incoming requests

--- src/shared/infra/http/api/initServer.ts
+++ src/shared/infra/http/api/initServer.ts
@@ -2,10 +2,11 @@ import Koa from 'koa'
 import bodyParser from 'koa-bodyparser'
 import helmet from 'koa-helmet'
 import cors from '@koa/cors'
-import v1Router from './v1Router'
+import morgan from 'koa-morgan'
 import { MikroORM, RequestContext } from '@mikro-orm/core'
 import logger from '@shared/infra/Logger/logger'
 import initErrorListener from './initErrorListener'
+import v1Router from './v1Router'
 
 export default (orm: MikroORM): Koa => {
   const app = new Koa()
@@ -14,12 +15,17 @@ export default (orm: MikroORM): Koa => {
     .use(bodyParser())
     .use(helmet())
     .use(cors())
+    .use(morgan('dev'))
     .use((ctx: Koa.Context, next: Koa.Next) => RequestContext.createAsync(orm.em, next))
     .use(v1Router.routes())
+    .use(v1Router.allowedMethods())
+
+  const port = 3000
+  app.listen(port).on('listening', () => {
+    logger.info(`Server is listening on port ${port}`)
+  })
 
   initErrorListener(app)
-
-  app.listen(4000, () => logger.info(`Server is listening on port 3000`))
 
   return app
 }
