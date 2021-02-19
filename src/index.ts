64993fcc1ffc71bbba0c876c3de20036d870d791
Daniel Kiss <kissd621@gmail.com>
2021-02-19 15:52:21 +0100

    Refactored code to delay module loading

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import initServer from './shared/infra/http/api/initServer'
 import { initDatabaseConnection } from './shared/infra/database/MikroORM/config/initDatabaseConnection'
 
 const initApplication = async () => {
   const orm = await initDatabaseConnection()
 
+  // Delay loading of modules that depend on the ORM
+  const initServer = require('./shared/infra/http/api/initServer').default
   initServer(orm)
 }
 
