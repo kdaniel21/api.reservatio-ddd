246dde0bd83abaa1188f8d87757a9a27f52e8c94
Daniel Kiss <kissd621@gmail.com>
2021-02-19 08:06:17 +0100

    Restructured imports of entry point

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import initDatabaseConnection from '@shared/infra/database/MikroORM/config/init'
-import initServer from '@shared/infra/http/api/init'
+import initServer from './shared/infra/http/api/initServer'
+import { initDatabaseConnection } from './shared/infra/database/MikroORM/config/initDatabaseConnection'
 
 const initApplication = async () => {
   const orm = await initDatabaseConnection()
