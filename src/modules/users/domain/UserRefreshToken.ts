d036c6f83e2885252cc787fc6db77793e5cd84d3
Daniel Kiss <kissd621@gmail.com>
2021-02-20 20:08:37 +0100

    Added userId property

--- src/modules/users/domain/UserRefreshToken.ts
+++ src/modules/users/domain/UserRefreshToken.ts
@@ -3,22 +3,30 @@ import { Result } from '@shared/core/Result'
 import { TokenEntity, TokenEntityOptions, TokenEntityProps } from '@shared/domain/TokenEntity'
 import UniqueID from '@shared/domain/UniqueID'
 
-export default class UserRefreshToken extends TokenEntity {
+export interface UserRefreshTokenProps extends TokenEntityProps {
+  userId: UniqueID
+}
+
+export default class UserRefreshToken extends TokenEntity<UserRefreshTokenProps> {
   static tokenOptions: TokenEntityOptions = {
     TOKEN_LENGTH: 30,
     EXPIRATION_HOURS: 30 * 24,
   }
 
-  private constructor(props: TokenEntityProps, id?: UniqueID) {
+  get userId(): UniqueID {
+    return this.props.userId
+  }
+
+  private constructor(props: UserRefreshTokenProps, id?: UniqueID) {
     super(props, id)
   }
 
-  static create(props?: TokenEntityProps, id?: UniqueID): ErrorOr<UserRefreshToken> {
+  static create(props?: UserRefreshTokenProps, id?: UniqueID): ErrorOr<UserRefreshToken> {
     const validPropsOrError = TokenEntity.validateProps(props, id, this.tokenOptions)
 
     if (validPropsOrError.isFailure()) return Result.fail(validPropsOrError.error)
 
-    const validProps = validPropsOrError.value as TokenEntityProps
+    const validProps = { ...props, ...validPropsOrError.value }
     const userPasswordResetToken = new UserRefreshToken(validProps, id)
     return Result.ok(userPasswordResetToken)
   }
