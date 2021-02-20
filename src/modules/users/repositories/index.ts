d18992d2a3f73623c6cce7e3371053d6d510f156
Daniel Kiss <kissd621@gmail.com>
2021-02-20 20:10:58 +0100

    Added repository export

--- src/modules/users/repositories/index.ts
+++ src/modules/users/repositories/index.ts
@@ -1,4 +1,7 @@
 import { entityManager } from '@shared/infra/database/MikroORM/config/initDatabaseConnection'
+import MikroRefreshTokenRepository from './implementations/MikroRefreshTokenRepository'
 import MikroUserRepository from './implementations/MikroUserRepository'
 
 export const userRepository = new MikroUserRepository(entityManager)
+
+export const refreshTokenRepository = new MikroRefreshTokenRepository(entityManager)
