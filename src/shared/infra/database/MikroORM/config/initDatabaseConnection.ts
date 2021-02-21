b95cca6f2044afd2166c505d7f220dff350ed11c
Daniel Kiss <kissd621@gmail.com>
2021-02-21 15:55:41 +0100

    Added termination if MikroORM could not connect to the DB

--- src/shared/infra/database/MikroORM/config/initDatabaseConnection.ts
+++ src/shared/infra/database/MikroORM/config/initDatabaseConnection.ts
@@ -5,10 +5,16 @@ export let mikroOrmInstance: MikroORM
 export let entityManager: EntityManager
 
 export const initDatabaseConnection = async (): Promise<MikroORM> => {
-  mikroOrmInstance = await MikroORM.init()
-  entityManager = mikroOrmInstance.em
+  try {
+    mikroOrmInstance = await MikroORM.init()
+    entityManager = mikroOrmInstance.em
 
-  logger.info('[MikroORM] Successfully connected to the DB!')
+    logger.info('[MikroORM] Successfully connected to the DB!')
 
-  return mikroOrmInstance
+    return mikroOrmInstance
+  } catch (err) {
+    logger.fatal(`[MikroORM]: Fatal Error - Could not connect to the database!`, err)
+    logger.fatal('Terminating application...')
+    process.exit()
+  }
 }
