242535afc2ceae20e37ab46e31e9316fd3aae4f1
Daniel Kiss <kissd621@gmail.com>
2021-02-21 20:28:18 +0100

    Implemented new methods

--- src/modules/users/repositories/implementations/MikroUserRepository.ts
+++ src/modules/users/repositories/implementations/MikroUserRepository.ts
@@ -1,26 +1,39 @@
-import { EntityManager, EntityRepository, Repository } from '@mikro-orm/core'
+import { EntityManager } from '@mikro-orm/core'
 import User from '@modules/users/domain/User'
 import MikroRefreshTokenEntity from '@modules/users/infra/database/MikroORM/entities/MikroRefreshTokenEntity'
 import MikroUserEntity from '@modules/users/infra/database/MikroORM/entities/MikroUserEntity'
+import UserMapper from '@modules/users/mappers/UserMapper'
 import UserRepository from '../UserRepository'
 
-@Repository(MikroUserEntity)
-export default class MikroUserRepository
-  extends EntityRepository<MikroUserEntity>
-  implements UserRepository {
-  constructor(entityManager: EntityManager) {
-    super(entityManager, MikroUserEntity)
+export default class MikroUserRepository implements UserRepository<MikroUserEntity> {
+  constructor(private em: EntityManager) {}
+
+  async findByEmail(email: string): Promise<User | null> {
+    return this.findOne({ email })
+  }
+
+  async findMany(where: Partial<MikroUserEntity>): Promise<User[]> {
+    const users = await this.em.find(MikroUserEntity, where)
+
+    return users.map(mikroUser => UserMapper.toDomain(mikroUser))
+  }
+
+  async findOne(where: Partial<MikroUserEntity>): Promise<User | null> {
+    const user = await this.em.findOne(MikroUserEntity, where)
+    if (!user) return null
+
+    return UserMapper.toDomain(user)
   }
 
   async existsByEmail(email: string): Promise<boolean> {
-    const count = await this.count({ email })
+    const count = await this.em.count(MikroUserEntity, { email })
 
     return !!count
   }
 
   async save(user: User): Promise<void> {
     const mikroUser = await this.toOrmEntity(user)
-    
+
     await this.em.persistAndFlush(mikroUser)
   }
 
