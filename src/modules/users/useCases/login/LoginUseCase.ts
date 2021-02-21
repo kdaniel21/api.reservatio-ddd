9f62fc456908dfe634d0cfaf3b01ced4e0d5b101
Daniel Kiss <kissd621@gmail.com>
2021-02-21 20:28:55 +0100

    Continued use case implementation

--- src/modules/users/useCases/login/LoginUseCase.ts
+++ src/modules/users/useCases/login/LoginUseCase.ts
@@ -3,9 +3,20 @@ import { ErrorOr } from '@shared/core/DomainError'
 import UserRepository from '@modules/users/repositories/UserRepository'
 import LoginDto from './LoginDto'
 import LoginResponseDto from './LoginResponseDto'
+import { Result } from '@shared/core/Result'
+import { LoginErrors } from './LoginErrors'
 
 export default class LoginUseCase implements UseCase<LoginDto, LoginResponseDto> {
   constructor(private userRepo: UserRepository) {}
 
-  async execute(request: LoginDto): Promise<ErrorOr<LoginResponseDto>> {}
+  async execute(request: LoginDto): Promise<ErrorOr<LoginResponseDto>> {
+    const { email, password } = request
+    const user = await this.userRepo.findByEmail(email)
+
+    const isUserFound = !!user
+    if (!isUserFound) return Result.fail(new LoginErrors.InvalidCredentialsError())
+
+    const isPasswordCorrect = await user.password.comparePassword(password)
+    if (!isPasswordCorrect) return Result.fail(new LoginErrors.InvalidCredentialsError())
+  }
 }
