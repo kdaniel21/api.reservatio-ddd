1f0c8a2f7450f51aeab60afc71f70e7dc3e8ad66
Daniel Kiss <kissd621@gmail.com>
2021-02-21 20:27:55 +0100

    Added more methods and better typings

--- src/modules/users/repositories/UserRepository.ts
+++ src/modules/users/repositories/UserRepository.ts
@@ -1,5 +1,7 @@
 import BaseRepository from '@shared/infra/BaseRepository'
+import User from '../domain/User'
 
-export default interface UserRepository extends BaseRepository {
+export default interface UserRepository<OrmE = any> extends BaseRepository<User, OrmE> {
   existsByEmail(email: string): Promise<boolean>
+  findByEmail(email: string): Promise<User | null>
 }
