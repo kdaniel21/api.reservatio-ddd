cd3aca14eb78bb2e377f97f842720b0c61ca0148
Daniel Kiss <kissd621@gmail.com>
2021-02-21 15:49:51 +0100

    Encapsulated subscription setup logic

--- src/modules/users/subscribers/AfterUserCreated.ts
+++ src/modules/users/subscribers/AfterUserCreated.ts
@@ -1,13 +1,10 @@
 import { DomainEvent } from '@shared/domain/events/DomainEvent'
-import DomainEvents from '@shared/domain/events/DomainEvents'
 import DomainEventSubscriber from '@shared/domain/events/DomainEventSubscriber'
 import UserCreatedEvent from '../domain/events/UserCreatedEvent'
 
-export default class AfterUserCreated implements DomainEventSubscriber {
-  constructor() {}
-
-  setupSubscriptions(): void {
-    DomainEvents.registerHandler(this.handleEvent.bind(this), UserCreatedEvent.name)
+export default class AfterUserCreated extends DomainEventSubscriber {
+  constructor() {
+    super(UserCreatedEvent.name)
   }
 
   async handleEvent(event: DomainEvent): Promise<void> {}
--- src/shared/domain/events/DomainEventSubscriber.ts
+++ src/shared/domain/events/DomainEventSubscriber.ts
@@ -1,6 +1,16 @@
+import logger from '@shared/infra/Logger/logger'
 import { DomainEvent } from './DomainEvent'
+import DomainEvents from './DomainEvents'
 
-export default interface DomainEventSubscriber {
-  setupSubscriptions(): void
-  handleEvent(event: DomainEvent): Promise<void> | void
+export default abstract class DomainEventSubscriber {
+  constructor(eventClassName: string) {
+    this.initSubscriber(eventClassName)
+  }
+
+  initSubscriber(eventClassName: string): void {
+    logger.info(`[EVENTS]: Initializing subscriber ${this.constructor.name}...`)
+    DomainEvents.registerHandler(this.handleEvent.bind(this), eventClassName)
+  }
+
+  abstract handleEvent(event: DomainEvent): Promise<void> | void
 }
