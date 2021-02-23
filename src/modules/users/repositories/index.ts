f9b0088430d4f9f36a21a0d57840ea008aa2e2b0
Daniel Kiss <kissd621@gmail.com>
2021-02-23 20:25:23 +0100

    Removed redundant code

--- src/modules/users/repositories/index.ts
+++ src/modules/users/repositories/index.ts
@@ -2,8 +2,6 @@ import prisma from '@shared/infra/database/prisma/prisma'
 import PrismaRefreshTokenRepository from './implementations/PrismaRefreshTokenRepository';
 import PrismaUserRepository from './implementations/PrismaUserRepository'
 
-// export const refreshTokenRepository = new MikroRefreshTokenRepository(entityManager)
-
 export const refreshTokenRepository = new PrismaRefreshTokenRepository(prisma)
 
 export const userRepository = new PrismaUserRepository(prisma, refreshTokenRepository)
