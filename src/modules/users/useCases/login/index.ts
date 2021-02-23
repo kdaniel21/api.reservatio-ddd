4ed65555367b37e654c998993d12eef3710d09cb
Daniel Kiss <kissd621@gmail.com>
2021-02-23 20:40:17 +0100

    Added dependency

--- src/modules/users/useCases/login/index.ts
+++ src/modules/users/useCases/login/index.ts
@@ -1,8 +1,12 @@
-import { userRepository } from '@modules/users/repositories'
+import { refreshTokenRepository, userRepository } from '@modules/users/repositories'
 import LoginUseCase from './LoginUseCase'
 import LoginController from './LoginController'
 import { authService } from '@modules/users/services'
 
-export const loginUseCase = new LoginUseCase(userRepository, authService)
+export const loginUseCase = new LoginUseCase(
+  userRepository,
+  refreshTokenRepository,
+  authService
+)
 
 export const loginController = new LoginController(loginUseCase)
