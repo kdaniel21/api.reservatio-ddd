83483ff48898a75b9d411beb1464e9dd482dfbbb
Daniel Kiss <kissd621@gmail.com>
2021-02-23 20:10:19 +0100

    Formatting

--- src/modules/users/repositories/RefreshTokenRepository.ts
+++ src/modules/users/repositories/RefreshTokenRepository.ts
@@ -1,4 +1,5 @@
 import BaseRepository from '@shared/infra/database/BaseRepository'
 import UserRefreshToken from '../domain/UserRefreshToken'
 
-export default interface RefreshTokenRepository<OrmE = any> extends BaseRepository<UserRefreshToken, OrmE> {}
+export default interface RefreshTokenRepository<OrmE = any>
+  extends BaseRepository<UserRefreshToken, OrmE> {}
