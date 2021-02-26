94e66ac98e4a8b1ddcc8a377153e435ffde8ab2b
Daniel Kiss <kissd621@gmail.com>
2021-02-26 18:52:48 +0100

    Added test cases for the future

--- src/modules/users/useCases/login/LoginUseCase.spec.ts
+++ src/modules/users/useCases/login/LoginUseCase.spec.ts
@@ -1,3 +1,8 @@
 describe('LoginUseCase', () => {
-  it('should login a use with valid credentials', () => {})
+  // it('should login a use with valid credentials', () => {})
+  // it('should give a valid refresh token after successful login', () => {})
+  // it('should persist the generated refresh token to the database', () => {})
+  // it('should give a valid access token after successful login', () => {})
+  // it('should throw InvalidCredentialsError if user does not exist', () => {})
+  // it('should throw InvalidCredentialsError if the password is incorrect', () => {})
 })
