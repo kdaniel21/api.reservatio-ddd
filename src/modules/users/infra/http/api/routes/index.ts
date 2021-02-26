3714cbed19f777c912ab875dd1a1baca6821301d
Daniel Kiss <kissd621@gmail.com>
2021-02-26 20:05:30 +0100

    Added register route

--- src/modules/users/infra/http/api/routes/index.ts
+++ src/modules/users/infra/http/api/routes/index.ts
@@ -2,6 +2,7 @@ import Koa from 'koa'
 import Router from 'koa-router'
 import { createUserController } from '@modules/users/useCases/createUser'
 import { loginController } from '@modules/users/useCases/login'
+import { registerController } from '@modules/users/useCases/register'
 
 const usersRouter = new Router()
 
@@ -9,5 +10,6 @@ usersRouter.prefix('/users')
 
 usersRouter.post('/', (ctx: Koa.Context) => createUserController.execute(ctx))
 usersRouter.post('/login', (ctx: Koa.Context) => loginController.execute(ctx))
+usersRouter.post('/register', (ctx: Koa.Context) => registerController.execute(ctx))
 
 export default usersRouter
