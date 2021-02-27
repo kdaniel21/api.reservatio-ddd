438a30f0ea86e1e80142b5158191b1e9674c0591
Daniel Kiss <kissd621@gmail.com>
2021-02-27 10:29:54 +0100

    Removed unused dependecy

--- src/modules/users/useCases/login/index.ts
+++ src/modules/users/useCases/login/index.ts
@@ -1,12 +1,8 @@
-import { refreshTokenRepository, userRepository } from '@modules/users/repositories'
+import { userRepository } from '@modules/users/repositories'
 import LoginUseCase from './LoginUseCase'
 import LoginController from './LoginController'
 import { authService } from '@modules/users/services'
 
-export const loginUseCase = new LoginUseCase(
-  userRepository,
-  refreshTokenRepository,
-  authService
-)
+export const loginUseCase = new LoginUseCase(userRepository, authService)
 
 export const loginController = new LoginController(loginUseCase)
