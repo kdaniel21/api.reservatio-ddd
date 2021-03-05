1a0f0f7b9f4aaec2951c739c762cb84fd21413eb
Daniel Kiss <kissd621@gmail.com>
2021-03-05 17:47:22 +0100

    Added select fields

--- src/shared/infra/database/BaseRepository.ts
+++ src/shared/infra/database/BaseRepository.ts
@@ -1,6 +1,7 @@
 export default interface BaseRepository<E, OrmE> {
-  findMany?(condition?: Partial<OrmE>): Promise<E[]>
-  findOne?(condition?: Partial<OrmE>): Promise<E | null>
+  findMany?(condition?: Partial<OrmE>, fields?: { [field: string]: boolean }): Promise<E[]>
+  findOne?(condition?: Partial<OrmE>, fields?: { [field: string]: boolean }): Promise<E | null>
   save?(entity: E): Promise<void>
+  deleteOne?(entity: E): Promise<void>
   toOrmEntity?(entity: E): Promise<OrmE> | OrmE
 }
