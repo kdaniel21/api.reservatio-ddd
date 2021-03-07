53276d876388d36f32066687d5723b0de3eee69e
Daniel Kiss <kissd621@gmail.com>
2021-03-07 12:40:54 +0100

    Restricted access

--- src/modules/users/infra/http/koa/routes/usersRouter.ts
+++ src/modules/users/infra/http/koa/routes/usersRouter.ts
@@ -1,11 +1,14 @@
 import Router from 'koa-router'
 import KoaContext from '@shared/infra/http/koa/KoaContext'
 import { createUserController } from '@modules/users/useCases/createUser'
+import { authMiddleware } from '@shared/infra/http/koa/middleware'
 
 const usersRouter = new Router()
 
 usersRouter.prefix('/users')
 
-usersRouter.post('/', (ctx: KoaContext) => createUserController.execute(ctx))
+usersRouter.post('/', authMiddleware.validateJwtFetchUserAndAdminOnly(), (ctx: KoaContext) =>
+  createUserController.execute(ctx)
+)
 
 export default usersRouter
