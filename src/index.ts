280f558a1694f3e05e43ccab86b5da960c860327
Daniel Kiss <kissd621@gmail.com>
2021-04-20 20:10:58 +0200

    Eslint fine tuning

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import logger from '@shared/infra/Logger/logger'
+/* eslint-disable global-require */
 import initApolloServer from '@shared/infra/http/apollo/initApolloServer'
+import logger from '@shared/infra/Logger/logger'
 
 const initApplication = async () => {
   logger.info('Starting application...')
